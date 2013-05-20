a3f4df177849ec5fc1d3dc2d8b71ec3075b7742c
Jorge Monforte González <yo@llou.net>
2013-05-20 20:09:03 +0200

    Working installation on macos

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from os import chmod
+from stat import S_IRWXU
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from distutils.errors import CompileError
@@ -8,14 +10,17 @@ class new_install(install):
 
     def run(self):
         install.run(self)
-        root = self.root if self.root is not None else ""
-        v =  call(['./install_libheracles', root])
+        # set executable to install-libheracles
+        chmod('install_libheracles', S_IRWXU)
+
+        options = [self.root] if self.root is not None else []
+        v =  call(['./install_libheracles'] + options)
         if v != 0:
             raise CompileError("Unable to compile and install libheracles")
         install.run(self)
 
 setup(name="heracles",
-      version="0.0.1",
+      version="0.0.2-1",
       author="Jorge Monforte",
       author_email="jorge.monforte@gmail.com",
       packages=find_packages(exclude=["test"]),
